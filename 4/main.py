4d3cbbe01b176a6993d259020a6327e7dcc22f5a
Omar Orga <orgaomar8@gmail.com>
2024-12-01 21:42:17 +0800

    update

--- 4/main.py
+++ 4/main.py
@@ -1,47 +1,19 @@
 import cv2 as cv
-import numpy as np
 import os
-import pyautogui
 from time import time
-from PIL import ImageGrab
-import win32gui
-import win32ui
-import win32con
+
+
 
 # Change the working directory to the folder this script is in.
 # Doing this because I'll be putting the files from each video in their own folder on GitHub
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-def window_capture():
-    w = 1920 # set this
-    h = 1080 # set this
-    bmpfilenamename = "out.bmp" #set this
-
-    hwnd = win32gui.FindWindow(None, windowname)
-    wDC = win32gui.GetWindowDC(hwnd)
-    dcObj=win32ui.CreateDCFromHandle(wDC)
-    cDC=dcObj.CreateCompatibleDC()
-    dataBitMap = win32ui.CreateBitmap()
-    dataBitMap.CreateCompatibleBitmap(dcObj, w, h)
-    cDC.SelectObject(dataBitMap)
-    cDC.BitBlt((0,0),(w, h) , dcObj, (0,0), win32con.SRCCOPY)
-    dataBitMap.SaveBitmapFile(cDC, bmpfilenamename)
-
-    # Free Resources
-    dcObj.DeleteDC()
-    cDC.DeleteDC()
-    win32gui.ReleaseDC(hwnd, wDC)
-    win32gui.DeleteObject(dataBitMap.GetHandle())
-
-
+# list_window_name()
+# exit()
 
 loop_time = time()
 while(True):
-    screenshot = 1
-
-    screenshot = np.array(screenshot)
-    screenshot = cv.cvtColor(screenshot, cv.COLOR_RGB2BGR)
-    print(screenshot)
+    screenshot = window_capture()
     cv.imshow('Computer vision', screenshot)
     
     print('FPS {}'.format(1 / (time() - loop_time)))
---
+++ 4/windowcapture.py
@@ -0,0 +1,55 @@
+import numpy as np
+import win32gui, win32ui, win32con
+
+class window_capture:
+
+    def get_screenshot():
+        w = 1920 # set this
+        h = 1080 # set this
+        bmpfilenamename = "out.bmp"  #set this
+        windowname = 'Grass'
+
+        hwnd = win32gui.FindWindow(None, windowname)
+        
+        wDC = win32gui.GetWindowDC(hwnd)
+        dcObj=win32ui.CreateDCFromHandle(wDC)
+        cDC=dcObj.CreateCompatibleDC()
+        dataBitMap = win32ui.CreateBitmap()
+        dataBitMap.CreateCompatibleBitmap(dcObj, w, h)
+        cDC.SelectObject(dataBitMap)
+        cDC.BitBlt((0,0),(w, h) , dcObj, (0,0), win32con.SRCCOPY)
+        
+        # save image
+        # dataBitMap.SaveBitmapFile(cDC, bmpfilenamename)
+        signedIntsArray = dataBitMap.GetBitmapBits(True)
+        img = np.frombuffer(signedIntsArray, dtype=np.uint8)
+        img.shape = (h, w, 4)
+
+        # Free Resources
+        dcObj.DeleteDC()
+        cDC.DeleteDC()
+        win32gui.ReleaseDC(hwnd, wDC)
+        win32gui.DeleteObject(dataBitMap.GetHandle())
+        
+        # drop the alpha channel, or cv.matchTemplate() will throw an error like:
+        #   error: (-215:Assertion failed) (depth == CV_8U || depth == CV_32F) && type == _templ.type() 
+        #   && _img.dims() <= 2 in function 'cv::matchTemplate'
+        img = img[...,:3]
+
+        # make image C_CONTIGUOUS to avoid errors that look like:
+        #   File ... in draw_rectangles
+        #   TypeError: an integer is required (got type tuple)
+        # see the discussion here:
+        # https://github.com/opencv/opencv/issues/14866#issuecomment-580207109
+        img = np.ascontiguousarray(img)
+        
+        return img
+    
+    def list_window_name():
+    # find the name of the window you're interested in.
+    # once you have it, update window_capture()
+    # https://stackoverflow.com/questions/55547940/how-to-get-a-list-of-the-name-of-every-open-window
+        def winEnumHandler(hwnd, ctx):
+            if win32gui.IsWindowVisible(hwnd):
+                print(hex(hwnd), win32gui.GetWindowText(hwnd))
+        win32gui.EnumWindows(winEnumHandler, None)
