b5f0498299481d5ff645b4ca256110b6020d16fa
Omar Orga <orgaomar8@gmail.com>
2024-12-01 21:09:30 +0800

    "Removed WindowCapture class and replaced with new window capture implementation using pyautogui and PIL; updated main.py to use new implementation"

--- 4/main.py
+++ 4/main.py
@@ -1,34 +1,58 @@
 import cv2 as cv
 import numpy as np
 import os
+import pyautogui
 from time import time
-from windowcapture import WindowCapture
+from PIL import ImageGrab
+from grab_screen import grab_screen
+import win32gui
+import win32ui
+import win32con
+
+
 
 # Change the working directory to the folder this script is in.
 # Doing this because I'll be putting the files from each video in their own folder on GitHub
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
-# initialize the WindowCapture class
-wincap = WindowCapture('OpenCV: Template Matching - Google Chrome')
+def window_capture():
+    w = 1920 # set this
+    h = 1080 # set this
+    bmpfilenamename = "out.bmp" #set this
+
+    hwnd = win32gui.FindWindow(None, windowname)
+    wDC = win32gui.GetWindowDC(hwnd)
+    dcObj=win32ui.CreateDCFromHandle(wDC)
+    cDC=dcObj.CreateCompatibleDC()
+    dataBitMap = win32ui.CreateBitmap()
+    dataBitMap.CreateCompatibleBitmap(dcObj, w, h)
+    cDC.SelectObject(dataBitMap)
+    cDC.BitBlt((0,0),(w, h) , dcObj, (0,0), win32con.SRCCOPY)
+    dataBitMap.SaveBitmapFile(cDC, bmpfilenamename)
+
+    # Free Resources
+    dcObj.DeleteDC()
+    cDC.DeleteDC()
+    win32gui.ReleaseDC(hwnd, wDC)
+    win32gui.DeleteObject(dataBitMap.GetHandle())
+
 
 loop_time = time()
 while(True):
+    screenshot = grab_screen(region=(1280, 0, 3840, 1440))
 
-    # get an updated image of the game
-    screenshot = wincap.get_screenshot()
-
-    cv.imshow('Computer Vision', screenshot)
-
-    # debug the loop rate
+    screenshot = np.array(screenshot)
+    screenshot = cv.cvtColor(screenshot, cv.COLOR_RGB2BGR)
+    print(screenshot)
+    cv.imshow('Computer vision', screenshot)
+    
     print('FPS {}'.format(1 / (time() - loop_time)))
     loop_time = time()
-
     # press 'q' with the output window focused to exit.
-    # waits 1 ms every loop to process key presses
+    # waitKey(1) will delay for 1ms every loop to process the key press
     if cv.waitKey(1) == ord('q'):
         cv.destroyAllWindows()
         break
 
 print('Done.')
-wincap.list_window_names()
